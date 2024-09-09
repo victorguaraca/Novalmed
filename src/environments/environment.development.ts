096ab9bc5deef02a0c513574880b933b49889ca8
victorguaraca <139037634+victorguaraca@users.noreply.github.com>
2024-09-09 02:52:20 -0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    URL_BACKEND: 'http://18.221.57.30',
-    URL_SERVICIOS: 'http://18.221.57.30/api',
+    URL_BACKEND: 'https://18.221.57.30',
+    URL_SERVICIOS: 'https://18.221.57.30/api',
     URL_FRONTEND: 'http://localhost:4200/'
 };
