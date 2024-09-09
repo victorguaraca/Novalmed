49b33b25dedc2fc849514d081673977c503aaae1
VictorDenov <victor.guaracaa@gmail.com>
2024-09-08 22:38:21 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    URL_BACKEND: 'https://api.cursos-udemy.site',
+    URL_BACKEND: 'http://18.221.57.30',
     URL_SERVICIOS: 'http://18.221.57.30/api',
     URL_FRONTEND: 'https://admin.cursos-udemy.site/'
 };
