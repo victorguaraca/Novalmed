ccb981ccfb1bf04420fe37c73f1003f64d0cd879
VictorDenov <victor.guaracaa@gmail.com>
2024-09-08 22:34:40 -0500

    update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    URL_BACKEND: 'http://127.0.0.1:8000',
-    URL_SERVICIOS: 'http://127.0.0.1:8000/api',
+    URL_BACKEND: 'http://18.221.57.30',
+    URL_SERVICIOS: 'http://18.221.57.30/api',
     URL_FRONTEND: 'http://localhost:4200/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     URL_BACKEND: 'https://api.cursos-udemy.site',
-    URL_SERVICIOS: 'https://api.cursos-udemy.site/api',
+    URL_SERVICIOS: 'http://18.221.57.30/api',
     URL_FRONTEND: 'https://admin.cursos-udemy.site/'
 };
