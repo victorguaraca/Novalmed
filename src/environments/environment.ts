32b8ff93c69c1cd62d8069ec4cc7c130fb990eb1
victorguaraca <139037634+victorguaraca@users.noreply.github.com>
2024-09-09 03:03:27 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     URL_BACKEND: 'https://18.221.57.30',
     URL_SERVICIOS: 'https://18.221.57.30/api',
-    URL_FRONTEND: 'https://admin.cursos-udemy.site/'
+    URL_FRONTEND: 'https://novalmed14.netlify.app/'
 };
